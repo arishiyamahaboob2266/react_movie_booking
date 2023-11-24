3701f36126398ed83059ab619862f89dd9b9dd29
arishiyamahaboob2266 <emailofarishiya@gmail.com>
2023-11-24 11:48:18 +0530

    my fourth commit

--- src/movieList/booking/SelectTime.js
+++ src/movieList/booking/SelectTime.js
@@ -2,22 +2,22 @@ import React,{useState}from 'react'
 
 const TimeSlotSelector=()=>{
     const[selectedTime,SetSelectedTime]=useState(null);
-}
+
 //time slot
 const availableTimeSlots=['10:00 AM','11:00 AM','2:00 AM','3:00 AM'];
 const handleTimeSlotSelect=(time)=>{
-    SetSelectedTime(time);
-};
-return(
-    
+      SetSelectedTime(time);
+    };
+ 
+    return(
     <div>
         <h2>Select a Time Slot</h2>
         <ul>
-            {availableTimeSlots.map((time)=>
-            (<li key={time} onClick={()=>handleTimeSlotSelect(time)}>{time}</li>
+            {availableTimeSlots.map((time)=>(<li key={time} onClick={()=>handleTimeSlotSelect(time)}>{time}</li>
 ))}
         </ul>
-        {selectedTime&&<p>Seleected time:{SelectedTime}</p>}
+        {selectedTime&&<p>Seleected time:{selectedTime}</p>}
     </div>
 )
+};
             
